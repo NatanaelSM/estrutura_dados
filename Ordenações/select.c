e0156e73f3f1d65336c062cf5a447b8d87661b90
Natanael <natanaelsm0102@gmail.com>
2026-04-27 20:48:29 -0300

    fix: select atualizado

--- Ordenações/select.c
+++ Ordenações/select.c
@@ -3,24 +3,25 @@
 int main()
 {
 
-    int vetor[] = {5, 8, 9, 0, 3, 4, 6};
-    int valorMaior, indexMenor;
+	int vetor[] = {5,3,1,4,0};
+	int length = sizeof(vetor) / sizeof(vetor[0]);
+	int indexMenorValor, valorAntigo;
 
-    for(int x = 0; x <= 5; x++){
-        indexMenor = x;
-        for(int y = x+1; y <= 6; y++){
-            if(vetor[y] < vetor[indexMenor]){
-                indexMenor = y;
-            }
-        };
-        valorMaior = vetor[x];
-        vetor[x] = vetor[indexMenor];
-        vetor[indexMenor] = valorMaior;
-    };
+	for(int i = 0; i < length; i++) {
+		indexMenorValor = i;
+		for(int j = i + 1; j < length; j++) {
+			if(vetor[j] < vetor[indexMenorValor]) {
+				indexMenorValor = j;
+			}
+		}
+		valorAntigo = vetor[i];
+		vetor[i] = vetor[indexMenorValor];
+		vetor[indexMenorValor] = valorAntigo;
+	}
 
-    for(int z = 0; z <= 6; z++){
-        printf("%d ", vetor[z]);
-    }
+	for(int y = 0; y < length; y++) {
+		printf("%d", vetor[y]);
+	}
 
-    return 0;
+	return 0;
 }

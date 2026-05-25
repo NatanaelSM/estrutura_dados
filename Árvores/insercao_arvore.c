f63798c31b0c9191c36da877e1f300c82210627d
Natanael <natanaelsm0102@gmail.com>
2026-05-25 20:21:48 -0300

    feat: algoritimo de inserção de árvore

--- Árvores/insercao_arvore.c
+++ Árvores/insercao_arvore.c
@@ -2,62 +2,34 @@
 #include <stdlib.h>
 
 typedef struct No {
-	int valor;
-	struct No *left_no;
-	struct No *right_no;
+    int valor;
+    struct No *left_no;
+    struct No *right_no;
 } No;
 
-No* comparar(No *noComparacao, int valorInserir) {
-	No *aux = noComparacao;
-
-	if(aux -> valor >= valorInserir) {
-		if(aux -> left_no == NULL) {
-			return &aux -> left_no;
-		}
-		aux = aux -> left_no;
-		comparar(aux, valorInserir);
-	}else{
-        if(aux -> right_no == NULL) {
-		    return &aux -> right_no;
-	    }
-        aux = aux -> right_no;
-	    comparar(aux,valorInserir);
+No* insere(No *ptr, int valorInserir) {
+    if (ptr == NULL) {
+        No *novoNo = malloc(sizeof(No));
+        novoNo->valor = valorInserir;
+        novoNo->left_no = NULL;
+        novoNo->right_no = NULL;
+        return novoNo;
     }
-    
+    if (valorInserir < ptr->valor) {
+        ptr->left_no = insere(ptr->left_no, valorInserir);
+    } else if (valorInserir > ptr->valor) {
+        ptr->right_no = insere(ptr->right_no, valorInserir);
+    }
+    return ptr;
 }
 
-
-void insere(No **raiz, int valorInserir) {
-
-	No *novoNo = malloc(sizeof(No));
-
-	if(!novoNo) {
-		printf("Erro ao alocar memória!");
-		return;
-	}
+int main() {
+    No *raiz = NULL;
+    raiz = insere(raiz, 10);
+    raiz = insere(raiz, 5);
+    raiz = insere(raiz, 7);
+    raiz = insere(raiz, 13);
+    raiz = insere(raiz, 17);
     
-    novoNo -> valor = valorInserir;
-	novoNo -> left_no = NULL;
-	novoNo -> right_no = NULL;
-
-	if(*raiz == NULL) {
-		*raiz = novoNo;
-        printf("Raiz adicionada! %d", (*raiz) -> valor);
-        return;
-	}
-
-    novoNo = comparar(*raiz, valorInserir);
-    printf("Valor %d inserido com sucesso!\n", novoNo->valor);
-}
-
-int main()
-{
-	No *raiz = NULL;
-	insere(&raiz, 10);
-	insere(&raiz, 5);
-	insere(&raiz, 7);
-	insere(&raiz, 13);
-	insere(&raiz, 17);
-
-	return 0;
+    return 0;
 }

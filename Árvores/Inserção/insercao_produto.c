5994a768f45e9f5f5f90d5348e57f5d3b2d921e4
Natanael <natanaelsm0102@gmail.com>
2026-05-27 19:32:35 -0300

    feat: remoção folha e inserção.

---
+++ Árvores/Inserção/insercao_produto.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct No {
+    int codigo;
+    double valor;
+    char nome[256];
+    struct No *left_no;
+    struct No *right_no;
+} Produto;
+
+Produto* inserirProduto(Produto **ptr, int codigoProduto, double valorProduto, char nomeProduto[]) {
+    
+    if(*ptr == NULL){
+        Produto *novoProduto = malloc(sizeof(Produto));
+        novoProduto -> codigo = codigoProduto;
+        novoProduto -> valor = valorProduto;
+        strcpy(novoProduto->nome, nomeProduto);
+        novoProduto->left_no = NULL;
+        novoProduto->right_no = NULL;
+        *ptr = novoProduto;
+    }
+    if(codigoProduto < (*ptr) -> codigo){
+        (*ptr) -> left_no = inserirProduto(&(*ptr) -> left_no, codigoProduto, valorProduto, nomeProduto);
+    }
+    if(codigoProduto > (*ptr) -> codigo){
+        (*ptr) -> right_no = inserirProduto(&(*ptr) -> right_no, codigoProduto, valorProduto, nomeProduto);
+    }
+
+    return *ptr;
+
+};
+
+void escolherAcao(Produto **raiz){
+    int numAcao = -1;
+
+    while(numAcao != 0){
+
+        printf("Digite um dos valores a seguir para realizar uma ação:\n1 - Adicionar produto\n0 - Sair\n");
+        scanf("%d", &numAcao);
+
+        switch(numAcao){
+            case 1:
+                int codigoProduto;
+                double valorProduto;
+                char nomeProduto[256];
+
+                printf("Digite o código do produto:\n");
+                scanf("%d", &codigoProduto);
+
+                printf("Digite o valor do produto:\n");
+                scanf("%lf", &valorProduto);
+
+                printf("Digite o nome do produto:\n");
+                scanf(" %[^\n]", nomeProduto);
+
+                inserirProduto(raiz, codigoProduto, valorProduto, nomeProduto);
+
+                break;
+            case 0:
+                printf("Programa finalizado!\n");
+                break;
+
+            default:
+                printf("Opção inválida!\n");
+                break;
+
+        };
+    };
+}
+
+int main() {
+    
+    Produto *raiz = NULL;
+    escolherAcao(&raiz);
+    
+    return 0;
+}
---
+++ Árvores/Remoção/remocao_folha.c.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No *left_no;
+    struct No *right_no;
+} No;
+
+No* insere(No *ptr, int valorInserir) {
+    if (ptr == NULL) {
+        No *novoNo = malloc(sizeof(No));
+        novoNo->valor = valorInserir;
+        novoNo->left_no = NULL;
+        novoNo->right_no = NULL;
+        return novoNo;
+    }
+    if (valorInserir < ptr->valor) {
+        ptr->left_no = insere(ptr->left_no, valorInserir);
+    } else if (valorInserir > ptr->valor) {
+        ptr->right_no = insere(ptr->right_no, valorInserir);
+    }
+    return ptr;
+}
+
+No* remover(No **ptr, int valorRemover){
+    if(*ptr == NULL){
+        return NULL;
+    }else{
+        if(valorRemover == (*ptr) -> valor && (*ptr) -> left_no == NULL && (*ptr) -> right_no == NULL){
+            free(*ptr);
+            return NULL;
+        }else if(valorRemover < (*ptr) -> valor){
+            (*ptr) -> left_no = remover(&(*ptr) -> left_no, valorRemover);
+        }else{
+            (*ptr) -> right_no = remover(&(*ptr) -> right_no, valorRemover);
+        }
+        return *ptr;
+    }
+}
+
+void imprime(No *raiz){
+    if(raiz !=NULL){
+        imprime(raiz -> left_no);
+        printf("%d ", raiz -> valor);
+        imprime(raiz -> right_no);
+    }
+}
+
+int main() {
+    No *raiz = NULL;
+    raiz = insere(raiz, 10);
+    raiz = insere(raiz, 5);
+    raiz = insere(raiz, 7);
+    raiz = insere(raiz, 13);
+    raiz = insere(raiz, 17);
+    raiz = insere(raiz, 1);
+    
+    imprime(raiz);
+    printf("\n");
+    remover(&raiz, 1);
+    remover(&raiz, 17);
+    imprime(raiz);
+    
+    return 0;
+}
